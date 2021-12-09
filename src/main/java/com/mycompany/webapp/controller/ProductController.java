4cbe7177b239698e988ffa9dbccb17e1f62b2cc5
fxdmin <go.jminn@gmail.com>
2021-12-09 17:59:10 +0900

    [SKY-86] feat: 상품관리 수정중..

--- src/main/java/com/mycompany/webapp/controller/ProductController.java
+++ src/main/java/com/mycompany/webapp/controller/ProductController.java
@@ -1,8 +1,14 @@
 package com.mycompany.webapp.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.mycompany.webapp.dto.Auth;
+import com.mycompany.webapp.dto.Products;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,8 +26,21 @@ public class ProductController {
 	
 	//상품조회
 	@RequestMapping("/list")
-	public String productList() {
+	public String productList(Model model, HttpSession session) {
 		log.info("실행");
+		
+		Auth auth = new Auth();
+		auth.setJwt(
+				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzkxMjYwMDgsIm1pZCI6Im1pZDEiLCJhdXRob3JpdHkiOiJST0xFX1VTRVIifQ.lW5znR6F9Zdl8G20TRWeVi33n-EiX6eJ6-RHIOSn7Gk");
+		auth.setMid("mid1");
+		
+		WebClient webClient = WebClient.create();
+		
+		Products productList = webClient.get().uri("http://localhost:82/product/list/1")
+				.header("Authorization", "Bearer "+ auth.getJwt()).retrieve().bodyToMono(Products.class).block();
+		
+		model.addAttribute("products", productList.getProducts());
+		System.out.println("#####"+productList.getProducts());
 		return "product/productList";
 	}
 	
---
+++ src/main/java/com/mycompany/webapp/dto/Products.java
@@ -0,0 +1,10 @@
+package com.mycompany.webapp.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Products {
+	public List<Product> products;
+}
