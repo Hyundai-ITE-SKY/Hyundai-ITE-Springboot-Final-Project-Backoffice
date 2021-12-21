2603f14ab9143522ebf8ee1d66ca5180cf90c2da
chaeyeon4u <cy990426@naver.com>
2021-12-21 19:29:23 +0900

    [SKY-68] feat: 대시보드 데이터 바인딩 진행중

--- src/main/java/com/mycompany/webapp/controller/HomeController.java
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -10,7 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mycompany.webapp.dto.Auth;
+import com.mycompany.webapp.dto.IntegerVariable;
+import com.mycompany.webapp.dto.OrderPerDays;
+import com.mycompany.webapp.dto.Products;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,7 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 public class HomeController {
 
 	@RequestMapping("/")
-	public String home(HttpSession session) {
+	public String home(HttpSession session, Model model) {
 		log.info("실행");
 
 		Auth auth = (Auth) session.getAttribute("auth");
@@ -27,7 +31,39 @@ public class HomeController {
 		if (auth == null || auth.getResult().equals("fail")) {
 			return "redirect:/loginform";
 		}
+		
+		WebClient webClient = WebClient.create();
+		//총 매출액
+		IntegerVariable totalPrice = webClient.get().uri("http://localhost:82/dash/totalprice")
+				.header("Authorization", "Bearer "+ auth.getJwt()).retrieve().bodyToMono(IntegerVariable.class).block();
+		model.addAttribute("totalPrice", totalPrice.getValue());
+		
+		//이번달 매출액
+		IntegerVariable monthPrice = webClient.get().uri("http://localhost:82/dash/monthprice")
+				.header("Authorization", "Bearer "+ auth.getJwt()).retrieve().bodyToMono(IntegerVariable.class).block();
+		model.addAttribute("monthPrice", monthPrice.getValue());
+		
+		//금일 매출액
+		IntegerVariable todayPrice = webClient.get().uri("http://localhost:82/dash/todayprice")
+				.header("Authorization", "Bearer "+ auth.getJwt()).retrieve().bodyToMono(IntegerVariable.class).block();
+		model.addAttribute("todayPrice", todayPrice.getValue());
+		
+		//날짜별 주문수
+		OrderPerDays orderPerDays = webClient.get().uri("http://localhost:82/dash/order/day")
+				.header("Authorization", "Bearer "+ auth.getJwt()).retrieve().bodyToMono(OrderPerDays.class).block();
+		model.addAttribute("orderPerDays", orderPerDays.getOrderPerDays());
+		log.info("orderPerDays : "+orderPerDays.getOrderPerDays());
+		
+		//날짜별 주문자수
+		OrderPerDays orderPerDayUser = webClient.get().uri("http://localhost:82/dash/order/dayuser")
+				.header("Authorization", "Bearer "+ auth.getJwt()).retrieve().bodyToMono(OrderPerDays.class).block();
+		model.addAttribute("orderPerDayUser", orderPerDayUser.getOrderPerDays());
+		log.info("orderPerDayUser : "+orderPerDayUser.getOrderPerDays());
 
+		//월별, 카테고리별 매출
+		String MonthTotalPrice = webClient.get().uri("http://localhost:82/dash/monthtotalprice")
+									.header("Authorization", "Bearer "+ auth.getJwt()).retrieve().bodyToMono(String.class).block();
+		model.addAttribute("monthTotalPrice", MonthTotalPrice);
 		return "home";
 	}
 
